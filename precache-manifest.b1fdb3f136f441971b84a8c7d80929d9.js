4051308574f6d82f74950c9e6ddc012e7be68d4d
Joseph Rehfuss <josephrehfuss@L10241-FVFXVBL0HV22.domain>
2019-10-30 20:46:54 -0700

    Updates

---
+++ precache-manifest.b1fdb3f136f441971b84a8c7d80929d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57281387123849ba562e12e6009c3913",
+    "url": "/manateepi_frontend/index.html"
+  },
+  {
+    "revision": "3c110f60e51c7ff285ef",
+    "url": "/manateepi_frontend/static/css/main.95515ff1.chunk.css"
+  },
+  {
+    "revision": "8db8502d3226d48607a5",
+    "url": "/manateepi_frontend/static/js/2.7fec6a92.chunk.js"
+  },
+  {
+    "revision": "3c110f60e51c7ff285ef",
+    "url": "/manateepi_frontend/static/js/main.d2a26dd9.chunk.js"
+  },
+  {
+    "revision": "ae04e67c90e0fa3445a8",
+    "url": "/manateepi_frontend/static/js/runtime-main.1d00e0b3.js"
+  }
+]);
